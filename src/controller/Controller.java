25ea1a5ac6f1997fc26c53f5987072ba52c6b18b
Jens Wouters <Jens@10.92.224.38>
2016-11-27 17:19:09 +0100

    push missing files

---
+++ src/controller/Controller.java
@@ -0,0 +1,37 @@
+package controller;
+
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import view.ZeeslagFrame;
+
+public class Controller {
+private ZeeslagFrame view;
+	
+	public Controller(ZeeslagFrame view){
+		this.view = view;
+		view.getBoard1().addMouseClickListener(new MouseClickHandler());
+	}
+	
+	private class MouseClickHandler extends MouseAdapter{
+		 public void mouseClicked(MouseEvent event){
+			 int x = event.getX();
+			 int y = event.getY();
+			 int nr = -1;
+			 for (int i = 0; i < view.getBoard1().getVierkanten().size(); i++){
+					if (view.getBoard1().getVierkanten().get(i).isAangeklikt(x, y)){
+						nr = i;
+						if (!view.getBoard1().getVierkanten().get(i).getBezet()){
+							view.getBoard1().setKleur(nr, Color.LIGHT_GRAY);
+						}
+						else{
+							view.getBoard1().setKleur(nr, Color.YELLOW);
+						}
+						view.getBoard2().setKleur(nr, Color.RED);
+						break;						
+					}	
+			 }
+		 }
+	 }
+}
---
+++ src/domain/Vierkant.java
@@ -0,0 +1,48 @@
+package domain;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+
+public class Vierkant {
+	private int x,y,zijde;
+	private Color kleur;
+	public boolean bezet;
+	
+	public Vierkant(int x, int y, int zijde, Color kleur) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.zijde = zijde;
+		this.kleur = kleur;
+		bezet = false;
+	}
+	public Color getKleur() {
+		return kleur;
+	}
+	public void setKleur(Color kleur) {
+		this.kleur = kleur;
+	}
+	public boolean getBezet(){
+		return bezet;
+	}
+	
+	public void paint(Graphics g) {
+		g.setColor( kleur );
+		g.fillRect( x, y, zijde, zijde );
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setStroke(new BasicStroke(3));
+	    g.setColor(Color.BLACK);
+	    g.drawRect( x, y, zijde, zijde );
+	}
+	
+	public boolean isAangeklikt(int x,int y){
+		boolean aangeklikt = this.x <= x && this.x+zijde >= x &&
+			    this.y <= y && this.y+ zijde >= y;	
+		if (aangeklikt){
+			bezet = !bezet;
+		}	   
+		return aangeklikt;		
+	}
+}
