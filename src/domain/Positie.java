38b94e442430f81b6709f20bc08198f3a56fad86
Simon Diaz <simon.diazaviles@student.ucll.be>
2016-12-16 16:19:22 +0100

    Positie, Richting, Serviece

---
+++ src/domain/Positie.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Positie {
+	
+	int x, y;
+	
+	public Positie(int x, int y)
+	{
+		
+	}
+}
---
+++ src/domain/ServiceInterface.java
@@ -0,0 +1,9 @@
+package domain;
+
+public interface ServiceInterface {
+
+	public plaatsSchip(Richting, schip, Positie positie)
+	{
+		
+	}
+}
